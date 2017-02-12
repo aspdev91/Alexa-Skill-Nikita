60d1f18eae9b83fd3814b01b7ed34dd8b96ea53f
aspdev91 <mtgmusic1@gmail.com>
2017-02-12 14:00:42 -0500

    'updated skills for news and weather'

--- index.js
+++ index.js
@@ -6,8 +6,9 @@
 
 const Alexa = require('alexa-sdk');
 const request = require('superagent');
+// var AWS = require('aws-sdk');
 
-const APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
+const APP_ID = 'amzn1.ask.skill.ed442aba-79c5-456b-b663-90edbbcf2efe';  // TODO replace with your app ID (OPTIONAL).
 
 const handlers = {
     'LaunchRequest': function () {
@@ -16,18 +17,51 @@ const handlers = {
     'CallNikita': function () {
         this.emit('GetFact');
     },
-    'GetFact': function () {
+    'Unhandled': function () {
+        this.emit(':ask', "I couldn't process that");
+    },
+    'news': function () {
 
-        request.get('https://awesomo.top/nikita')
+        request.get('https://awesomo.top/nikita?command=news+on+techcrunch')
             .end((err, res) => {
                 if (err) {
                     console.log("There's been an error.")
                     console.log(err)
                 };
                 console.log(res)
-                let speechOutput = res.result;
-                this.emit(':tellWithCard', "Nikita's Response", speechOutput);
+                let newsSource = res.body.result.source;
+                let articles = res.body.result.articles
+                this.emit(':tellWithCard', readNews(articles, newsSource));
             })
+
+        function readNews(articles, newsSource) {
+            let allArticles = 'This is whats happening on ' + newsSource + ' . . . .';
+            let count = 0
+            for (var article of articles) {
+                if (count >= 3) {
+                    break
+                }
+                allArticles += article.title;
+                allArticles += '.   .   .   .   .'
+                allArticles += article.description;
+                allArticles += ' next up . . .'
+                count += 1
+            }
+
+            return allArticles
+        }
+    },
+    'weather': function () {
+
+        request.get('https://awesomo.top/nikita?command=weather+in+new+york')
+            .end((err, res) => {
+                if (err) {
+                    console.log("There's been an error.")
+                    console.log(err)
+                };
+                this.emit(':tellWithCard', res.body.result);
+            })
+
     },
     'AMAZON.HelpIntent': function () {
         const speechOutput = this.t('HELP_MESSAGE');
@@ -49,7 +83,7 @@ exports.handler = (event, context) => {
     const alexa = Alexa.handler(event, context);
     alexa.APP_ID = APP_ID;
     // To enable string internationalization (i18n) features, set a resources object.
-    alexa.resources = languageStrings;
+    // alexa.resources = languageStrings;
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
